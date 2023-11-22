1d8e6ccae3a7ffa1ba3e2a5e72fb5f8d6b04d3d5
Olmedann <60108774+Olmedann@users.noreply.github.com>
2023-11-22 14:40:21 -0600

    Update __init__.py
    Nueva función

--- minimal/__init__.py
+++ minimal/__init__.py
@@ -6,3 +6,13 @@ def my_sum(x, y):
 def my_mul(x, y):
     """A function that multiply. """
     return x*y
+
+def palabras(cadena):
+    diccionario = dict()
+    palabras = cadena.split(" ")
+    for i in palabras:
+        if i in diccionario.keys():
+            diccionario[i] = diccionario[i]+1
+        else:
+                diccionario[i] = 1
+    return diccionario
